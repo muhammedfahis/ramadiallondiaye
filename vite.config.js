d01430ad52c32fa9b3c9016b79d17d7ca3a28224
Mohammed Fayis <mohammedfayis@MacBook-Pro.local>
2025-12-02 22:57:55 +0530

    cc

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
         about: 'about.html',
         vision: 'vision.html',
         contact: 'contact.html',
+        gallery: 'gallery.html',
+        projects: 'projects.html',
       },
     },
   },
